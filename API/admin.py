5ce808dff00ab7f48d13e0401a4abb079e45252b
simizzz <merander207203@gmail.com>
2023-06-05 23:06:02 +0500

    Изменил Админ панель

--- API/admin.py
+++ API/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Competence
+from .models import Competence, Trajectory, User
 # Register your models here.
 
 admin.site.register(Competence)
-
+admin.site.register(Trajectory)
+admin.site.register(User)
