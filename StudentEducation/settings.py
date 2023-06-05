3a2ba7c172f4214c3c37d676832ce90ead3db07e
simizzz <merander207203@gmail.com>
2023-06-05 23:01:51 +0500

    Изменил Setting файл

--- StudentEducation/settings.py
+++ StudentEducation/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "StudentEducation.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASE_URL = "postgresql://postgres:vEQaLwlFKZBnagSBCanz@containers-us-west-91.railway.app:7954/railway"
+DATABASE_URL = "postgresql://postgres:SoTBI9Be8qBdNMAWNyo4@containers-us-west-47.railway.app:6095/railway"
 
 DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
