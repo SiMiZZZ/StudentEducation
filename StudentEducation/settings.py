c8ce8e3ac68af89facb39b81b6dac08608b93dc3
simizzz <merander207203@gmail.com>
2023-07-23 02:37:29 +0500

    Изменил конфиги

--- StudentEducation/settings.py
+++ StudentEducation/settings.py
@@ -82,10 +82,17 @@ WSGI_APPLICATION = "StudentEducation.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASE_URL = "postgresql://postgres:SoTBI9Be8qBdNMAWNyo4@containers-us-west-47.railway.app:6095/railway"
+DATABASE_URL = "postgresql://postgres:8KpQRkwHy44r1PTX3Ixy@containers-us-west-88.railway.app:5504/railway"
 
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'education_db',
+'USER': 'postgres',
+'PASSWORD': 'admin',
+'HOST': '127.0.0.1',
+'PORT': '5432'
+}
 }
 
 
